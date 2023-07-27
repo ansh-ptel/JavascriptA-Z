effb1e83506079b7c53a49b57dc8ca145dae6171
Anshu Patel <anshupatel@Anshus-MacBook-Pro.local>
2023-07-27 15:28:39 +0530

    Coding Challenge 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -154,6 +154,7 @@ Template Literals
 woow`);
 */
 
+/*
 const age = 15;
 
 if (age >= 18) {
@@ -172,3 +173,22 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+*/
+
+// Coding Challenge 2 
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+    console.log(`"John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
